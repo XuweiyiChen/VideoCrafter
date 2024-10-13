c1f7891191a09284cd11a78a9037c98ec2e46504
Tian Xia <tianxia@umich.edu>
2024-10-13 12:53:02 -0400

    change dir

--- scripts/evaluation/run.sh
+++ scripts/evaluation/run.sh
@@ -7,10 +7,14 @@
 #SBATCH --cpus-per-task=4  # Increased CPU count for parallel processing
 #SBATCH --mem=40G  # Adjust as per your job's requirement
 #SBATCH --gres=gpu:1  # Adjust based on GPU needs
-#SBATCH --job-name="VideoCrafter_freeinit_I=3_seed=0"
+#SBATCH --job-name="VideoCrafter_freeinit_I=3_seed=0_1024_v1"
 #SBATCH --output=/scratch/lsa_root/lsa1/tianxia/VideoCrafter/gl-outputs/%x-%j.out
 #SBATCH --error=/scratch/lsa_root/lsa1/tianxia/VideoCrafter/gl-outputs/%x-%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL  # Removed BEGIN and REQUEUE notifications
 module load cuda
 cd /scratch/lsa_root/lsa1/tianxia/VideoCrafter
-python scripts/evaluation/infer.py --seed 0 --savedir results/VideoCrafter_freeinit_I=3_seed=0
+python scripts/evaluation/infer.py --prompt_file prompts/test_prompts.txt \
+                                   --ckpt_path checkpoints/base_1024_v1/model.ckpt \
+                                   --config configs/inference_t2v_1024_v1.0.yaml \
+                                   --seed 0 --height 320 --width 512\
+                                   --savedir Test/freeinit_model=1024_res=320*512__seed=0
